55afaf2338b6ffb2740fc56987244ed12c6f8289
Victor Salinas <victorsalinas6@gmail.com>
2023-12-08 10:38:57 -0600

    Declared the valuables and interpolatized them

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Victor";
+            int age = 30;
+            char initial = 'V';
+            bool isAlive = true;
+            double number = 30.52;
+            decimal littleNumber = 0.93745m;
+
+            Console.WriteLine($"My name is {name} I´m {age} years old, my initial is {initial}, Its {isAlive} that I´m Alive, " +
+                $"My technical age is {number} and my typing speed is {littleNumber}");
         }
     }
 }
